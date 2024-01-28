989939db99d6d402fbbf85295f07fa8c593b6f38
Xoan Carreira <xoancano@gmail.com>
2024-01-29 00:53:25 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", {
+
+
+
+
+
+
+});
+
+
+
+
+
+const presentacion = document.getElementById("presentacion");
+const habilidades = document.getElementById("habilidades");
+const proyectos = document.getElementById("proyectos");
+const formacion = document.getElementById("formacion");
+
+let seccions = [presentacion, habilidades, proyectos, formacion];
+
+function mostrar(item){
+    for (let i = 0; i < seccions.length; i++) {
+        seccions[i].style.width = "0px";  
+    }
+    item.style.width = "600px";
+}
+
